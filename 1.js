9ff7268e6a5cf397bb4c7598c35f3baeeae8b0be
MUX <14107519+muxfox@users.noreply.github.com>
2026-01-03 17:14:31 +0530

    Add JSON response structure in 1.js

---
+++ 1.js
@@ -0,0 +1,11 @@
+{
+  "success" : true,
+  "statusCode" : 200,
+  "message" : "Hacked By SAM",
+  "data" : {
+    "loginAllowed" : 1,
+    "remainingTrialDays" : 200,
+    "userId" : "1ef88023-bbff-40b5-919e-6501208a5a29",
+    "isPaid" : true
+  }
+}
